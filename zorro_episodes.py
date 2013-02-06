d56c3af1e680eb20a70dee76a82405168264b866
Davide F <ancestrall@gmail.com>
2013-02-06 15:50:21 +0200

    calculate zorro heuristic

---
+++ zorro_episodes.py
@@ -0,0 +1,45 @@
+def parseEpisodes(path):
+	with open(path) as f:
+		content = f.readlines()
+	lines = [l.split() for l in content]
+	listOfCategories = [el[1] for el in lines]
+	return listOfCategories
+
+def countHeusticTDD(events):
+	#events = events[::-1]  #reverse the list AND returns it
+	TFCounter = 0
+	TLCounter = 0 
+	TF = False
+	TL = False
+	while(events):
+		el = events.pop()
+		if el == "test-first":
+			TFCounter +=1
+			TF=True
+			TL=False
+		elif el == "test-last":
+			TLCounter+=1
+			TF=False
+			TL=True
+		else:
+			if TF:
+				TFCounter+=1
+			else:
+				TLCounter+=1
+	result = {"TF": TFCounter, "TL": TLCounter}
+	return result
+
+
+	# for i, ev in events:
+	# 	if ev=="test-first"
+	# 		start = i
+
+def main():
+	cats  = parseEpisodes("C:\Users\dfucci\Desktop\SQAT\zorroEpisodes.txt")
+	denominator = len(cats)
+	res = countHeusticTDD(cats)
+	numerator = float(res["TF"])
+	print "Conformance level: {0:.0f}%".format(numerator/denominator*100)
+
+if __name__ == "__main__":
+	main()
